fd48981373beff0fe990b2b0a9b2f5e6a70b435f
adamclawrence <lawrence.adam99@gmail.com>
2015-10-29 17:21:52 -0400

    add Test Folder
    added test folder with the beginnings of my gamble JUnit test.

---
+++ src/Test/JUnit/gambleTest.java
@@ -0,0 +1,19 @@
+package JUnit;
+import Java.Controllers.PubController;
+import org.junit.Before;
+import org.junit.Test;
+/**
+ * Created by Adam Lawrence on 10/29/2015.
+ */
+public class gambleTest {
+    @Before
+    public void setUp() {
+        PubController p = new PubController();
+
+    }
+
+    @Test
+    public void functionTest(PubController p) {
+        
+    }
+}
