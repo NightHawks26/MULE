371e8a186a6290259c84adfb32d3c4983a02d7b5
Adam Lawrence <lawrence.adam99@gmail.com>
2015-11-30 12:12:21 -0500

    change Gamble
    changed gambling so it is a bit more dynamic in terms of returned
    values.

--- src/Java/Objects/Pub.java
+++ src/Java/Objects/Pub.java
@@ -19,33 +19,34 @@ public class Pub {
     //0-12=50
     public int gamble(int pTime, int round) {
         Random randy = new Random();
+        int rando = randy.nextInt(51);
         int timeBonus = 0;
         int roundBonus = 0;
         if (pTime >= 37 && pTime <= 50) {
-            timeBonus = 200;
+            timeBonus = 75 + rando;
         } else if (pTime <= 37 && pTime >= 25) {
-            timeBonus = 150;
+            timeBonus =50 + rando;
         } else if (pTime <= 25 && pTime >= 12) {
-            timeBonus = 100;
+            timeBonus = 25 + rando;
         } else {
-            timeBonus = 50;
+            timeBonus = rando;
         }
 
         if (round > 11) {
-            roundBonus = 200;
+            roundBonus = 75 + rando;
         } else if (round <= 11 && round >= 8) {
-            roundBonus = 150;
+            roundBonus = 50 + rando;
         } else if (round < 8 && round >= 4 ) {
-            roundBonus = 100;
+            roundBonus = 25 + rando;
         } else {
-            roundBonus = 50;
+            roundBonus = rando;
         }
         //I feel like the formula posted is wrong(what is beneath this)
         //it will almost always give out 250. the minimum round bonus is
         //50. and the random number is between 0 and 250.
         //so unless the number is 4 or lower, it will always be an output
         //of 250 (the limit)
-        int bonus = roundBonus * randy.nextInt(timeBonus + 1);
+        int bonus = roundBonus + timeBonus;
         System.out.println(bonus);
         if (bonus > 250){
             bonus = 250;
